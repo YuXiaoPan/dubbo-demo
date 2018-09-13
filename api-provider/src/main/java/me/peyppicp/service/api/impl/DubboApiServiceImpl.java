549582595dd57606db7830332be99e414ebfa453
YuXiaoPan <peyppicp.pan@kikatech.com>
2018-09-13 20:32:36 +0800

    测试dubbo上下文api

--- api-provider/src/main/java/me/peyppicp/service/api/impl/DubboApiServiceImpl.java
+++ api-provider/src/main/java/me/peyppicp/service/api/impl/DubboApiServiceImpl.java
@@ -3,6 +3,9 @@ package me.peyppicp.service.api.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import me.peyppicp.api.DubboApiService;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author YuXiao Pan
  * @date 2018/9/12
@@ -13,6 +16,11 @@ public class DubboApiServiceImpl implements DubboApiService {
 
     @Override
     public String helloWorld() {
-        return "hello world!";
+        try {
+            return InetAddress.getLocalHost().getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
